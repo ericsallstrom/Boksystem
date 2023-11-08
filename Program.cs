e7b89176fcd597b4e63c176d0ed0b574efcfdd63
OlofEnar <41821535+OlofEnar@users.noreply.github.com>
2023-11-08 15:31:00 +0100

    Lägger till böcker

--- Program.cs
+++ Program.cs
@@ -5,14 +5,16 @@ namespace Boksystem
     internal class Program
     {
 
-
         static void Main(string[] args)
         {
             List<Bok> books = new List<Bok>();
 
+            books.Add(new Bok("Iron flame", "Rebecca Yarros", 12345678));
+            books.Add(new Bok("Egendom", "Hernan Diaz", 12345678));
+            books.Add(new Bok("Norrsken ", "Carl Bergstrand", 12345678));
+            books.Add(new Bok("Dune", "Alf", 12345678));
             books.Add(new Bok("Dune", "Alf", 12345678));
             books.Add(new EBok("Dune", "Alf", 12345678, 650, "test.com"));
-
 
         }
     }
