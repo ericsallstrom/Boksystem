46e4931b97f6b73f2e072f1604c6b66c76c78cc1
OlofEnar <41821535+OlofEnar@users.noreply.github.com>
2023-11-08 15:25:30 +0100

    Testing

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Boksystem
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Boksystem
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
-            
+            List<Bok> books = new List<Bok>();
+
+            books.Add(new Bok("Dune", "Alf", 12345678));
+            books.Add(new EBok("Dune", "Alf", 12345678, 650, "test.com"));
+
+
+
         }
     }
 }
